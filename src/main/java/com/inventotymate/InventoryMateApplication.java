9e6ff60ab5dc9b81f74b6cf4eec79dc3c8f863c8
Esteban <estebancalcha@gmail.com>
2025-02-15 12:41:48 -0500

    Initial commit - Backend InventoryMate

---
+++ src/main/java/com/inventotymate/InventoryMateApplication.java
@@ -0,0 +1,13 @@
+package com.inventotymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMateApplication.class, args);
+	}
+
+}
